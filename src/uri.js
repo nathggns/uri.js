6bdf9387421e42c3f51d14948743792a778f3eb2
Nathaniel Higgins <nat@nath.is>
2013-06-18 19:35:22 +0100

    Use two spaces

--- src/uri.js
+++ src/uri.js
@@ -1,158 +1,158 @@
 (function(undefined) {
 
-    var URI = {
+  var URI = {
 
-        /**
-         * Parse URI query strings
-         * @param  {string} query_string The query string to parse
-         * @param  {bool}   decode       Should values be URI decoded?
-         * @return {object}              The decoded query string
-         */
-        query: function(query_string, decode) {
-            // Default decode to true
-            if (typeof decode === 'undefined') decode = true;
+    /**
+     * Parse URI query strings
+     * @param  {string} query_string The query string to parse
+     * @param  {bool}   decode     Should values be URI decoded?
+     * @return {object}        The decoded query string
+     */
+    query: function(query_string, decode) {
+      // Default decode to true
+      if (typeof decode === 'undefined') decode = true;
 
-            // Replace the starting ?, if it is there
-            query_string = query_string.replace(/^\?/, '');
+      // Replace the starting ?, if it is there
+      query_string = query_string.replace(/^\?/, '');
 
-            // Split the query string into key value parts
-            var parts = query_string.split('&');
+      // Split the query string into key value parts
+      var parts = query_string.split('&');
 
-            // Iniate the return value
-            var query = {};
+      // Iniate the return value
+      var query = {};
 
-            // Loop through each other the parts, splitting it into keys and values
-            for (var i = 0, l = parts.length; i < l; i++) {
-                var part = parts[i];
-                var key;
-                var val = '';
+      // Loop through each other the parts, splitting it into keys and values
+      for (var i = 0, l = parts.length; i < l; i++) {
+        var part = parts[i];
+        var key;
+        var val = '';
 
-                if (part.match('=')) {
-                    // If it is in the format key=val
-                    
-                    // Split into the key and value by the = symbol
-                    var part_parts = part.split('=');
+        if (part.match('=')) {
+          // If it is in the format key=val
+          
+          // Split into the key and value by the = symbol
+          var part_parts = part.split('=');
 
-                    // Assign key and value
-                    key = part_parts[0];
-                    val = part_parts[1];
-                } else {
-                    // If there is no value, just set the key to the full part
-                    key = part;
-                }
-
-                // If we actually have a value, URI decode it
-                if (val !== '' && decode) {
-                    val = decodeURIComponent(val);
-                }
-
-                // Assign to the return value
-                query[key] = val;
-            }
-
-            return query;
-        },
-
-        /**
-         * Deep merge two or more objects
-         * @param {boolean} deep Should this be a deep copy or not?
-         * @return {object}      Merged version of the arguments
-         */
-        extend: function() {
-
-            // If we don't have enough arguments to do anything, just return an object
-            if (arguments.length < 2) {
-                return {};
-            }
-
-            // Argument shuffling
-            if (typeof arguments[0] !== 'boolean') {
-                Array.prototype.unshift.call(arguments, true);
-            }
-
-            // Remove the variables we need from the arguments stack.
-            deep = Array.prototype.shift.call(arguments);
-            var one = Array.prototype.shift.call(arguments);
-            var two = Array.prototype.shift.call(arguments);
-
-            // If we have more than two objects to merge
-            if (arguments.length > 0) {
-
-                // Push two back on to the arguments stack, it's no longer special.
-                Array.prototype.unshift.call(arguments, two);
-
-                // While we have any more arguments, call extend with the initial obj and the next argument
-                while (arguments.length > 0) {
-                    two = Array.prototype.shift.call(arguments);
-
-                    if (typeof two !== 'object') continue;
-
-                    one = arguments.callee(deep, one, two);
-                }
-
-                return one;
-            }
-
-            // Do some checking to force one and two to be objects
-            if (typeof one !== 'object') {
-                one = {};
-            }
-
-            if (typeof two !== 'object') {
-                two = {};
-            }
-
-            // Loop through the second object to merge it with the first
-            for (var key in two) {
-                // If this key actually belongs to the second argument
-                if (typeof two.hasOwnProperty === 'undefined' || two.hasOwnProperty(key)) {
-                    var current = two[key];
-
-                    if (deep && typeof current === 'object' && typeof one[key] === 'object') {
-                        // Deep copy
-                        one[key] = arguments.callee(one[key], current);
-                    } else {
-                        one[key] = current;
-                    }
-                }
-            }
-
-            return one;
+          // Assign key and value
+          key = part_parts[0];
+          val = part_parts[1];
+        } else {
+          // If there is no value, just set the key to the full part
+          key = part;
         }
-    }
 
-    if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
-        // CommonJS, Node, PhantomJS, etc modules
-        module.exports = exports = URI;
-    } else if (typeof define !== 'undefined') {
-        // RequireJS Module
-        define(function(require, exports, module) {
-            module.exports = exports = URI;
-        });
-    } else {
-        // Being included directly in the browser
-        window.URI = URI;
-        
-        var location = window.location;
-        var defaults = {
-            auto: {
-                query: false
-            },
-
-            keys: {
-                query: 'query'
-            }
-        };
-
-        if (!location.query_opts) {
-            location.query_opts = {};
-        };
-
-        var opts = URI.extend({}, defaults, location.query_opts);
-        delete location['query_opts'];
-
-        if (opts.auto || (typeof opts.auto === 'object' && opts.auto.query)) {
-            location[opts.keys.query] = URI.query(location.search);
+        // If we actually have a value, URI decode it
+        if (val !== '' && decode) {
+          val = decodeURIComponent(val);
         }
+
+        // Assign to the return value
+        query[key] = val;
+      }
+
+      return query;
+    },
+
+    /**
+     * Deep merge two or more objects
+     * @param {boolean} deep Should this be a deep copy or not?
+     * @return {object}    Merged version of the arguments
+     */
+    extend: function() {
+
+      // If we don't have enough arguments to do anything, just return an object
+      if (arguments.length < 2) {
+        return {};
+      }
+
+      // Argument shuffling
+      if (typeof arguments[0] !== 'boolean') {
+        Array.prototype.unshift.call(arguments, true);
+      }
+
+      // Remove the variables we need from the arguments stack.
+      deep = Array.prototype.shift.call(arguments);
+      var one = Array.prototype.shift.call(arguments);
+      var two = Array.prototype.shift.call(arguments);
+
+      // If we have more than two objects to merge
+      if (arguments.length > 0) {
+
+        // Push two back on to the arguments stack, it's no longer special.
+        Array.prototype.unshift.call(arguments, two);
+
+        // While we have any more arguments, call extend with the initial obj and the next argument
+        while (arguments.length > 0) {
+          two = Array.prototype.shift.call(arguments);
+
+          if (typeof two !== 'object') continue;
+
+          one = arguments.callee(deep, one, two);
+        }
+
+        return one;
+      }
+
+      // Do some checking to force one and two to be objects
+      if (typeof one !== 'object') {
+        one = {};
+      }
+
+      if (typeof two !== 'object') {
+        two = {};
+      }
+
+      // Loop through the second object to merge it with the first
+      for (var key in two) {
+        // If this key actually belongs to the second argument
+        if (typeof two.hasOwnProperty === 'undefined' || two.hasOwnProperty(key)) {
+          var current = two[key];
+
+          if (deep && typeof current === 'object' && typeof one[key] === 'object') {
+            // Deep copy
+            one[key] = arguments.callee(one[key], current);
+          } else {
+            one[key] = current;
+          }
+        }
+      }
+
+      return one;
     }
+  }
+
+  if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
+    // CommonJS, Node, PhantomJS, etc modules
+    module.exports = exports = URI;
+  } else if (typeof define !== 'undefined') {
+    // RequireJS Module
+    define(function(require, exports, module) {
+      module.exports = exports = URI;
+    });
+  } else {
+    // Being included directly in the browser
+    window.URI = URI;
+    
+    var location = window.location;
+    var defaults = {
+      auto: {
+        query: false
+      },
+
+      keys: {
+        query: 'query'
+      }
+    };
+
+    if (!location.query_opts) {
+      location.query_opts = {};
+    };
+
+    var opts = URI.extend({}, defaults, location.query_opts);
+    delete location['query_opts'];
+
+    if (opts.auto || (typeof opts.auto === 'object' && opts.auto.query)) {
+      location[opts.keys.query] = URI.query(location.search);
+    }
+  }
 })();
--- tests/uri.js
+++ tests/uri.js
@@ -3,74 +3,74 @@ var should = require('should');
 
 describe('URI', function() {
 
-    describe('query', function() {
+  describe('query', function() {
 
-        it('should have query method', function() {
-            URI.should.have.property('query');
-        });
-
-        it('should parse key=val', function() {
-            var parsed = URI.query('a=b');
-            parsed.should.eql({ a: 'b' })
-        });
-
-        it('should parse key', function() {
-            var parsed = URI.query('a');
-            parsed.should.eql({ a: '' })
-        });
-
-        it('should parse key and key=val', function() {
-            var parsed = URI.query('a&b=c');
-            parsed.should.eql({ a: '', b: 'c' })
-        });
-
-        it('should parse key and key=val with ? at front', function() {
-            var parsed = URI.query('?a&b=c');
-            parsed.should.eql({ a: '', b: 'c' })
-        });
-
-        it('should be decode values', function() {
-            var parsed = URI.query('?a=%3Fa%3Db');
-            parsed.should.eql({ a: '?a=b'});
-        });
-
-        it('should be able to disable decoding values', function() {
-            var parsed = URI.query('?a=%3Fa%3Db', false);
-            parsed.should.eql({ a: '%3Fa%3Db'});
-        });
-        
+    it('should have query method', function() {
+      URI.should.have.property('query');
     });
 
-    describe('extend', function() {
-        it('should merge two objects', function() {
-            var merged = URI.extend({ a: 'b' }, { c: 'd' });
-            merged.should.eql({ a: 'b', c: 'd' })
-        });
-
-        it('should deep merge two objects', function() {
-            var merged = URI.extend({ a: { b: 'c' }}, { a: { d: 'f' }});
-            merged.should.eql({ a: { b: 'c', d: 'f' }});
-        });
-
-        it('should be able to disable deep merge', function() {
-            var merged = URI.extend(false, { a: { b: 'c' }}, { a: { d: 'f' }});
-            merged.should.eql({ a: { d: 'f' }});
-        });
-
-        it('should merge more than 2', function() {
-            var merged = URI.extend({ a: 'b' }, { c: 'd' }, { e: 'f' });
-            merged.should.eql({ a: 'b', c: 'd', e: 'f' });
-        });
-
-        it('should deep merge more than 2', function() {
-            var merged = URI.extend({ a: { b: 'c' }}, { a: { d: 'e' }}, { a: { f: 'g' }});
-            merged.should.eql({ a: { b: 'c', d: 'e', f: 'g' }});
-        });
-
-        it('should be able to disable deep merge for more than 2', function() {
-           var merged = URI.extend(false, { a: { b: 'c' }}, { a: { d: 'e' }}, { a: { f: 'g' }});
-           merged.should.eql({ a: { f: 'g' }}); 
-       });
+    it('should parse key=val', function() {
+      var parsed = URI.query('a=b');
+      parsed.should.eql({ a: 'b' })
     });
+
+    it('should parse key', function() {
+      var parsed = URI.query('a');
+      parsed.should.eql({ a: '' })
+    });
+
+    it('should parse key and key=val', function() {
+      var parsed = URI.query('a&b=c');
+      parsed.should.eql({ a: '', b: 'c' })
+    });
+
+    it('should parse key and key=val with ? at front', function() {
+      var parsed = URI.query('?a&b=c');
+      parsed.should.eql({ a: '', b: 'c' })
+    });
+
+    it('should be decode values', function() {
+      var parsed = URI.query('?a=%3Fa%3Db');
+      parsed.should.eql({ a: '?a=b'});
+    });
+
+    it('should be able to disable decoding values', function() {
+      var parsed = URI.query('?a=%3Fa%3Db', false);
+      parsed.should.eql({ a: '%3Fa%3Db'});
+    });
+    
+  });
+
+  describe('extend', function() {
+    it('should merge two objects', function() {
+      var merged = URI.extend({ a: 'b' }, { c: 'd' });
+      merged.should.eql({ a: 'b', c: 'd' })
+    });
+
+    it('should deep merge two objects', function() {
+      var merged = URI.extend({ a: { b: 'c' }}, { a: { d: 'f' }});
+      merged.should.eql({ a: { b: 'c', d: 'f' }});
+    });
+
+    it('should be able to disable deep merge', function() {
+      var merged = URI.extend(false, { a: { b: 'c' }}, { a: { d: 'f' }});
+      merged.should.eql({ a: { d: 'f' }});
+    });
+
+    it('should merge more than 2', function() {
+      var merged = URI.extend({ a: 'b' }, { c: 'd' }, { e: 'f' });
+      merged.should.eql({ a: 'b', c: 'd', e: 'f' });
+    });
+
+    it('should deep merge more than 2', function() {
+      var merged = URI.extend({ a: { b: 'c' }}, { a: { d: 'e' }}, { a: { f: 'g' }});
+      merged.should.eql({ a: { b: 'c', d: 'e', f: 'g' }});
+    });
+
+    it('should be able to disable deep merge for more than 2', function() {
+       var merged = URI.extend(false, { a: { b: 'c' }}, { a: { d: 'e' }}, { a: { f: 'g' }});
+       merged.should.eql({ a: { f: 'g' }}); 
+     });
+  });
 
 });
